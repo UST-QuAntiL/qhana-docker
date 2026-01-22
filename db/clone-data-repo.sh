77c2ea96169ea42eb2170c7705475f4364de87a0
MaxSilvester <138241982+MaxSilvesterQWE@users.noreply.github.com>
2026-01-22 22:50:03 +0100

    qc-atlas-content: use latest version
    https://github.com/UST-QuAntiL/planqk-atlas-content/tree/d9701e986f7f7c3de52ee4072a853cfe9167d078

--- db/clone-data-repo.sh
+++ db/clone-data-repo.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-echo "cloning qc atlas data"
-
-# clone repo, if successful copy setup script to /docker-entrypoint-initdb.d/
-git clone --single-branch --branch ${QC_ATLAS_CONTENT_REPOSITORY_BRANCH} ${QC_ATLAS_CONTENT_REPOSITORY_URL} ${QC_ATLAS_CONTENT_REPOSITORY_PATH}
-if [ -d "${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${QC_ATLAS_SUBFOLDER_CONTENT_BACKUP_FILES}" ]; then
-    cp setup-atlas.sh /docker-entrypoint-initdb.d/
-    echo "${QC_ATLAS_CONTENT_REPOSITORY_BRANCH} of qc-atlas-content repo was cloned successfully"        
+# If the content repo is already mounted locally, use it without SSH.
+if [ -d "${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}" ]; then
+    echo "local content repo detected, initializing db from local files"
+    cp ${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}/* /docker-entrypoint-initdb.d/
+    exit 0
+fi
+# if ssh key is not empty, init the db with data:
+echo "checking if ssh key for the atlas content repo is present"
+FILE=/run/secrets/ssh_secret
+if test -f "$FILE"; then
+    echo "ssh key present"
+    # add ssh key and add github as known host
+    mkdir /root/.ssh/ && cp /run/secrets/ssh_secret /root/.ssh/id_rsa
+    chmod 400 /root/.ssh/id_rsa && ssh-keyscan github.com >> /root/.ssh/known_hosts
 else
-	    echo "unable to find specified directory with example data for qc-atlas in the repository"
+    echo "ssh key not present, proceeding without it"
+fi
+# clone repo and copy the file with the sql statements to the folder that is executed on startup of the postgres db
+git clone --single-branch --branch ${QC_ATLAS_CONTENT_REPOSITORY_BRANCH} ${QC_ATLAS_CONTENT_REPOSITORY_URL} ${QC_ATLAS_CONTENT_REPOSITORY_PATH}
+if [ -d "${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}" ]; then
+    cp ${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}/* /docker-entrypoint-initdb.d/
+else
+    echo "unable to find specified directory with example data in the repository"
 fi
