e00876f6eab64b2e8b9b6b20dbf2cd4711e64bee
MaxSilvester <138241982+MaxSilvesterQWE@users.noreply.github.com>
2026-04-19 22:02:41 +0200

    qc-atlas-db: switch to feature/quantum-clustering-low-code-model branch for the qunatum clustering low-code solution

--- qc-atlas-db/clone-data-repo.sh
+++ qc-atlas-db/clone-data-repo.sh
@@ -18,6 +18,7 @@ else
 fi
 # clone repo and copy the file with the sql statements to the folder that is executed on startup of the postgres db
 git clone --single-branch --branch ${QC_ATLAS_CONTENT_REPOSITORY_BRANCH} ${QC_ATLAS_CONTENT_REPOSITORY_URL} ${QC_ATLAS_CONTENT_REPOSITORY_PATH}
+(cd  ${QC_ATLAS_CONTENT_REPOSITORY_PATH}; git status; git show --no-patch)
 if [ -d "${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}" ]; then
     cp ${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}/* /docker-entrypoint-initdb.d/
 else
